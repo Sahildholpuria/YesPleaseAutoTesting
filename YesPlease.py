7874835ddbb1268c193a079592cdc023d24e0e02
Sahildholpuria <dholpuria1999@gmail.com>
2022-05-13 14:13:48 +0530

    code updated

--- YesPlease.py
+++ YesPlease.py
@@ -2,7 +2,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
-def YesPlease(emailAddress,Password):
+def YesPlease(emailAddress, Password):
     from selenium import webdriver
     webdriver = webdriver.Chrome(ChromeDriverManager().install())
     webdriver.get("https://www.yesplease.nl/home?lang=nl")
@@ -24,6 +24,23 @@ def YesPlease(emailAddress,Password):
     webdriver.find_element_by_xpath(
         "//body/div[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/button[1]").click()  # Login button
     time.sleep(10)
+    webdriver.find_element_by_xpath(
+        "//header/nav[1]/div[3]/div[1]/div[1]/nav[1]/div[2]/ul[1]/div[1]/li[1]/a[1]").click()
+    time.sleep(10)
+    webdriver.find_element_by_xpath("//a[@id='Onze producten']").click()
+    time.sleep()
+    for i in range(10):
+        webdriver.find_element_by_xpath(
+            "//body/div[1]/div[2]/div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[4]/center[1]/div[1]/div[3]/i[1]").click()
+
+    webdriver.find_element_by_xpath("//body/div[1]/div[2]/div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/button[1]").click()
+
+    
+    
+    
+    
+    
+    
     # webdriver.find_element_by_xpath(
     #     "//header/nav[1]/div[3]/div[1]/div[1]/nav[1]/div[2]/ul[1]/div[1]/li[1]/a[1]").click()  # Home Page
     # time.sleep(1)
